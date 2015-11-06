d4364fd21a5562fe27d21f3292ab9e90159e3c3c
Valery Frolov <valeryf@wix.com>
2015-11-07 00:36:15 +0200

    update project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tokenize "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a toy evaluator for lisp-like nested mathematical expressions"
+  :url "https://github.com/frolovv"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
